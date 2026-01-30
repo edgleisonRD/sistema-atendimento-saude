5c93f24fadf63838208b31db309a62b43c9c9fdc
Edgleison RD <edgleisontic@gmail.com>
2026-01-29 23:22:38 -0300

    Aplicação de polimorfismo em Enfermeiro e TecnicoEnfermagem

--- src/main/java/Model/Enfermeiro.java
+++ src/main/java/Model/Enfermeiro.java
@@ -1,4 +1,20 @@
 package Model;
 
-public class Enfermeiro{
+import java.util.Date;
+
+public class Enfermeiro extends ProfissionalSaude{
+    public Enfermeiro(int id, String nome, String cpf, Date dataNascimento, String telefone, int registroProfissional, String especialidade) {
+        super(id, nome, cpf, dataNascimento, telefone, registroProfissional, especialidade);
+    }
+
+    @Override
+    public Atendimento atenderPaciente(Paciente paciente) {
+
+        Atendimento atendimento = new Atendimento();
+        atendimento.setPaciente(paciente);
+        atendimento.setProfissional(this);
+        atendimento.setTipo("TRIAGEM");
+        atendimento.setDescricao("Triagem realizada, sinais vitais coletados e risco classificado.");
+        return atendimento;
+    }
 }
--- src/main/java/Model/Medico.java
+++ src/main/java/Model/Medico.java
@@ -2,8 +2,19 @@ package Model;
 
 import java.util.Date;
 
-public class Medico extends Pessoa{
-    public Medico(int id, String nome, String cpf, Date dataNascimento, String telefone) {
-        super(id, nome, cpf, dataNascimento, telefone);
+public class Medico extends ProfissionalSaude{
+    public Medico(int id, String nome, String cpf, Date dataNascimento, String telefone, int registroProfissional, String especialidade) {
+        super(id, nome, cpf, dataNascimento, telefone, registroProfissional, especialidade);
+    }
+
+    @Override
+    public Atendimento atenderPaciente(Paciente paciente) {
+        Atendimento atendimento = new Atendimento();
+        atendimento.setPaciente(paciente);
+        atendimento.setProfissional(this);
+        atendimento.setTipo("CONSULTA_MEDICA");
+        atendimento.setDescricao("Procedimento básico executado e apoio ao atendimento.");
+        return atendimento;
+
     }
 }
